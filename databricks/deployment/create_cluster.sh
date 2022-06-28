4555aa8525e3ecd00a7d8b27ae04f76489aef69b
krisbock <krbock@microsoft.com>
2022-06-28 14:57:12 +1000

    fixes #1

--- databricks/deployment/create_cluster.sh
+++ databricks/deployment/create_cluster.sh
@@ -11,6 +11,7 @@ DATABRICKS_CLUSTER_LOG='{
     }
 }'
 
+
 # Databricks Auth headers
 adbGlobalToken=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d --output json | jq -r .accessToken)
 azureApiToken=$(az account get-access-token --resource https://management.core.windows.net/ --output json | jq -r .accessToken)
